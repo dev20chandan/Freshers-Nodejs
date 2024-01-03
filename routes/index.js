6294cad3a30f40f28e86f014a554076b7ca30445
chandan sharma <real.it.expert@gmail.com>
2024-01-03 10:51:19 +0530

    make routing base end point

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+//here is the end point to navigate the url 
+router.get('/user',  async function (req, res) {
+    res.send("Hello User")
+})
 
 module.exports = router;
