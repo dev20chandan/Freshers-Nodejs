2ce6e01383c57b427fc16d0e54a6ed6686d40a06
chandan sharma <real.it.expert@gmail.com>
2024-01-05 18:25:55 +0530

    middleware make in route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const routes =require('./routes/index')
 const app = express();
-const port = 3000;
+const port = 4000;
 app.use('/',routes)
 // Start the server
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 // here is the import the file 
 const userController = require('../controller.js/userController')
-//here is the end point to navigate the url  
-// this both function work same but change the end point and make more efficient and clear the router  
-router.get('/user',  async function (req, res) {
-    res.send("Hello User")
-})
-// here is the use the function name user 
-router.get('/index',userController.user)
+
+//  this is making middleware for authentication
+const auth = (req, res,next) => {
+    if (req.isAuthenticated) {
+      return  next()
+    }
+    res.status(401).send("Unauthorized User")
+}
+
+// here is the use middleware name can be used
+router.get('/index',auth,userController.user)
 
 module.exports = router;
